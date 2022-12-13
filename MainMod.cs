5c395013a2e5fd97b167848cb70a4fbb7c6dfe00
Fozkais <brandon.fouy@gmail.com>
2022-12-13 23:09:01 +0100

    Initial version for CMS21 Multiplayer mod - Allow player to Init the connection with the Server (See serverConsole)

---
+++ MainMod.cs
@@ -0,0 +1,70 @@
+﻿using MelonLoader;
+
+namespace CMS21MP
+{
+    public class MainMod : MelonMod
+    {
+       public Client client;
+       public ModGUI modGUI;
+       
+        public override void OnApplicationStart() // Runs after Game Initialization.
+        {
+           
+        }
+
+        public override void OnApplicationLateStart() // Runs after OnApplicationStart.
+        {
+           client = new Client(); 
+           client.Initialize();
+           
+           modGUI = new ModGUI();
+           modGUI.Mod = this;
+           MelonLogger.Msg("Mod Initialized!");
+        }
+
+        public override void OnSceneWasLoaded(int buildindex, string sceneName) // Runs when a Scene has Loaded and is passed the Scene's Build Index and Name.
+        {
+           // MelonLogger.Msg("OnSceneWasLoaded: " + buildindex.ToString() + " | " + sceneName);
+        }
+
+        public override void OnSceneWasInitialized(int buildindex, string sceneName) // Runs when a Scene has Initialized and is passed the Scene's Build Index and Name.
+        {
+           // MelonLogger.Msg("OnSceneWasInitialized: " + buildindex.ToString() + " | " + sceneName);
+        }
+
+        public override void OnUpdate() // Runs once per frame.
+        {
+           // MelonLogger.Msg("OnUpdate");
+        }
+
+        public override void OnFixedUpdate() // Can run multiple times per frame. Mostly used for Physics.
+        {
+
+        }
+
+        public override void OnLateUpdate() // Runs once per frame after OnUpdate and OnFixedUpdate have finished.
+        {
+           modGUI.GuiOnUpdate();
+        }
+
+        public override void OnGUI() // Can run multiple times per frame. Mostly used for Unity's IMGUI.
+        {
+           modGUI.OnGUI();
+        }
+
+        public override void OnApplicationQuit() // Runs when the Game is told to Close.
+        {
+          //  MelonLogger.Msg("OnApplicationQuit");
+        }
+
+        public override void OnPreferencesSaved() // Runs when Melon Preferences get saved.
+        {
+           // MelonLogger.Msg("OnPreferencesSaved");
+        }
+
+        public override void OnPreferencesLoaded() // Runs when Melon Preferences get loaded.
+        {
+           // MelonLogger.Msg("OnPreferencesLoaded");
+        }
+    }
+}
---
+++ ModGUI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+
+namespace CMS21MP
+{
+    public class ModGUI
+    {
+        public static ModGUI instance;
+        public MainMod Mod;
+
+        public KeyCode MultiplayerGUIToggleKey = KeyCode.RightShift;
+        public bool isMultiplayerGUIShowed = false;
+
+
+        public string ipAdress = "127.0.0.1";
+        public string usernameField = "player";
+
+        public void OnGUI()
+        {
+            if (isMultiplayerGUIShowed)
+            {
+                int panelSizeX = 300;
+                int panelSizeY = 300;
+                float panelOffsetX = Screen.width - (panelSizeX + 25);
+                float panelOffsetY = 25;
+
+
+                GUI.Box(new Rect(panelOffsetX, panelOffsetY, panelSizeX, panelSizeY), "Multiplayer GUI");
+                if (GUI.Button(new Rect(panelOffsetX + 10, panelOffsetY + 50, panelSizeX - 20, 35), "Connect"))
+                {
+                    Mod.client.ip = ipAdress;
+                    Mod.client.ConnectToServer();
+                }
+                if (GUI.Button(new Rect(panelOffsetX + 10, panelOffsetY + 90, panelSizeX - 20, 35), "Host"))
+                {
+
+                }
+                
+                ipAdress = GUI.TextField(new Rect(panelOffsetX + 10, panelOffsetY + 130, panelSizeX - 20, 25), ipAdress);
+                usernameField = GUI.TextField(new Rect(panelOffsetX + 10, panelOffsetY + 160, panelSizeX - 20, 25), usernameField);
+            }
+        }
+
+        public void GuiOnUpdate()
+        {
+            if (Input.GetKeyDown(MultiplayerGUIToggleKey) && !isMultiplayerGUIShowed)
+            {
+                isMultiplayerGUIShowed = true;
+                MelonLogger.Msg("Multiplayer GUI Loaded!");
+            }
+            else if (Input.GetKeyDown(MultiplayerGUIToggleKey) && isMultiplayerGUIShowed)
+            {
+                isMultiplayerGUIShowed = false;
+                MelonLogger.Msg("Multiplayer GUI Unloaded!");
+            }
+        }
+    }
+}
