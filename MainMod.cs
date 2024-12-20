e9d43e29c9d113df23eb1ece1d481a12e661903c
Fozkais <brandon.fouy@gmail.com>
2024-12-20 16:44:08 +0100

    version assemble from 0.4.0 to 0.4.1

--- MainMod.cs
+++ MainMod.cs
@@ -21,7 +21,7 @@ namespace CMS21Together
 		public const int MAX_SAVE_COUNT = 22;
 		public const int MAX_PLAYER = 4;
 		public const int PORT = 7777;
-		public const string ASSEMBLY_MOD_VERSION = "0.4.0";
+		public const string ASSEMBLY_MOD_VERSION = "0.4.1";
 		public const string MOD_VERSION = "Together " + ASSEMBLY_MOD_VERSION;
 		public bool isModInitialized;
 
