f9c653252cd7f5b6786bda4eedfe7f8cb5f1f964
Fozkais <brandon.fouy@gmail.com>
2026-01-07 17:15:04 +0100

    Project Refacto for Early WIP Dedicated Server

---
+++ CMS21-Together-Server/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading;
+
+namespace CMS21_Together_Server
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Server is WIP!");
+			while (true)
+			{
+				Thread.Sleep(1000);
+			}
+		}
+	}
+}
